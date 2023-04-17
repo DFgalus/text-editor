0f0525918dd59615719e25a5485b5a9e4937bd8d
Dgalus <damiangalus7@gmail.com>
2023-04-17 15:30:05 -0400

    Created click event handler for install btn and added handler for appinstalled event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,15 +6,20 @@ window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     butInstall.style.visibility = 'visible';
 
-    butInstall.addEventListener('click', () => {
-        event.prompt();
-        butInstall.setAttribute('disabled', true);
-        butInstall.textContent = 'Installed!';
-    })
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async (event) => {
+    await event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    const header = document.createElement('<h2>');
+    header.innerText = 'App is already installed';
+    document.body.appendChild(header);
+    header.textContent = 'Successfully installed!';
+    console.log('👍', 'appinstalled', event);
+});
